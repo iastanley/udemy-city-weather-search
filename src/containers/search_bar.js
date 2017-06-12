6035c6d1ebf6316af209f90d639ddb4bab928fa5
iastanley <iasvks.craigslist@gmail.com>
2017-06-11 18:15:14 -0700

    implemented SearchBar container component

---
+++ src/containers/search_bar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchTerm: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <form className="input-group">
+        <input
+          value={this.state.searchTerm}
+          onChange={this.onInputChange}
+          className="form-control"/>
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">
+            Submit
+          </button>
+        </span>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
